f0f442fa80d0319c8580462359327a574e7aa3a6
Jason Grant <jason.grant@rubiconwater.com>
2022-11-27 14:44:11 +1100

    Move a creatsure

--- gol.py
+++ gol.py
@@ -1,43 +1,36 @@
-# These two libraries allow us to use the screen
+# The tkinter library allows us to use the screen
 # This library is described here: https://docs.python.org/3/library/tk.html
-# More info here: https://tkdocs.com/tutorial/index.html
-from tkinter import *
-from tkinter import ttk
+import tkinter as tk
+import time
 
-# A function to save the position in the event parameter to global variables
-def savePosn(event):
-    global lastx, lasty
-    lastx, lasty = event.x, event.y
-
-# A function to draw a line from the last position to the current
-# position (which is carried in the event parameter)
-def addLine(event):
-    canvas.create_line((lastx, lasty, event.x, event.y))
-    savePosn(event)
+# A function to move a creature
+def moveCreature(window, canvas, creature):
+    iterations = 20
+    x, y = 5, 5
+    while iterations > 0:
+        canvas.move(creature, x, y)
+        window.update()
+        iterations = iterations - 1
+        time.sleep(0.1)
 
 # ==========================================
 # The script starts executing below
 # ==========================================
 
-# Initialise the tk library
-rootWindow = Tk()
+rootWindow = tk.Tk()
 # Make the root window only have on column and row for placing child widgets
 rootWindow.columnconfigure(0, weight=1)
 rootWindow.rowconfigure(0, weight=1)
-
 # Create a canvas
-canvas = Canvas(rootWindow, width=1000, height=1000)
-
+canvas = tk.Canvas(rootWindow, width=1000, height=1000)
 # Place the canvas widget within the root window at row=0 and col=0
-canvas.grid(column=0, row=0, sticky=(N, W, E, S))
-
-# Tell the canvas that button presses and mouse motion will be handled by our functions
-canvas.bind("<Button-1>", savePosn)
-canvas.bind("<B1-Motion>", addLine)
-
+canvas.grid(column=0, row=0, sticky=(tk.N, tk.W, tk.E, tk.S))
 # Draw a border around the canvas
 # Creating items in tkinter is described here: https://tkdocs.com/tutorial/canvas.html#creating
 canvas.create_rectangle(1, 1, 1000, 1000, outline='red')
 
-# Now that we have set up the window, tell it to start processing
-rootWindow.mainloop()
+# Create a creature
+creature = canvas.create_oval(10, 10, 20, 20, outline='red')
+
+# Run the simulation
+moveCreature(rootWindow, canvas, creature)
