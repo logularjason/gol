bf2226ece32029c34669354c2d5740310283e4bb
Laura Grant <laura@logular.com>
2022-12-03 23:12:07 +1100

    creatures are self aware

--- gol.py
+++ gol.py
@@ -4,6 +4,19 @@ import tkinter as tk
 import time
 import random 
 
+# a class to model a creature 
+class Creature:
+    def __init__(self, canvas):
+        self.canvas = canvas
+        x = random.randint(0, 1000)
+        y = random.randint(0, 1000)
+        self.creature = canvas.create_oval(x , y, x+10, y+10, outline='red')
+
+    def move(self):
+        self.canvas.move(self.creature, 5, 5)
+
+
+
 # A function containing the initialisation logic
 # We put this into a function to keep the code tidy
 def createCanvas(window):
@@ -28,21 +41,18 @@ def createCreatures(canvas, creatureCount):
 # Each creature is created with a different location and appended to our list
 # See how it's possible to use '+' to append lists?
     for c in range(creatureCount):
-        x = random.randint(0, 1000)
-        y = random.randint(0, 1000)
-        creatures = creatures + [canvas.create_oval(x , y, x+10, y+10, outline='red')]
+        creatures = creatures + [Creature(canvas)]
     # Return the list to the caller
     return creatures
 
 # A function to move the list of creatures
 def moveCreatures(window, canvas, creatures):
     movementCount = 50
-    x, y = 5, 5
     # Move the creatures repeatedly
     while movementCount > 0:
         # Move each creature
         for creature in creatures:
-            canvas.move(creature, x, y)
+            creature.move()
         # Decrease counter and sleep
         window.update()
         movementCount = movementCount - 1
