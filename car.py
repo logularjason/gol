d6c588ca5f34a46373cd2dc11a69372a9dbb80c9
Jason Grant <jason.grant@rubiconwater.com>
2022-12-27 13:05:02 +1100

    Added battery and fuelSystem

--- car.py
+++ car.py
@@ -1,28 +1,70 @@
-class Car:
-    def __init__(self, model, tankCapacity):
-        self.model = model 
+class Battery:
+    def __init__(self):
+        self.charge = 12
+
+    def drive(self, km):
+        self.charge += km * 0.02
+        if self.charge > 13.8:
+            self.charge = 13.8
+
+    def park(self, months):
+        self.charge -= months * 0.05
+        if self.charge < 0:
+            self.charge = 0
+
+class FuelSystem:
+    def __init__(self, tankCapacity):
         self.tankCapacity = tankCapacity
         self.fuelLevel = 0
-
-    def print(self):
-        print(self.model, self.tankCapacity, self.fuelLevel, sep="---")
-
 
     def fill(self, amount):
         self.fuelLevel += amount
 
+    def drive(self, km, litersPerKm):
+        self.fuelLevel -= km * litersPerKm
 
-dadsCar = Car("toyota", 72)
-laurasCar = Car("porsche", 50)
+class Car:
+    def __init__(self, model, tankCapacity, litresPerKm):
+        self.model = model 
+        self.litresPerKm = litresPerKm
+        self.battery = Battery()
+        self.fuelSystem = FuelSystem(tankCapacity)
 
+    def print(self, message):
+        print(message, " model=", self.model, ", tankCapacity=", self.fuelSystem.tankCapacity, ", batteryCharge=", self.battery.charge, ", fuelLevel=", self.fuelSystem.fuelLevel, sep="")
 
-dadsCar.print()
-laurasCar.print()
+    def fill(self, amount):
+        self.fuelSystem.fill(amount)
 
+    def park(self, months):
+        self.battery.park(months)
+
+    def drive(self, km):
+        self.battery.drive(km)
+        self.fuelSystem.drive(km, self.litresPerKm)
+  
+
+dadsCar = Car("toyota", 72, 0.1)
+laurasCar = Car("porsche", 50, 0.05)
+
+dadsCar.print("Begin     ")
+laurasCar.print("Begin     ")
 
 dadsCar.fill(30)
 laurasCar.fill(50)
 laurasCar.fill(10)
 
-dadsCar.print()
-laurasCar.print()
+dadsCar.print("After fill")
+laurasCar.print("After fill")
+
+dadsCar.park(4)
+laurasCar.park(3)
+
+dadsCar.print("After park")
+laurasCar.print("After park")
+
+dadsCar.drive(200)
+laurasCar.drive(1)
+
+dadsCar.print("After driv")
+laurasCar.print("After driv")
