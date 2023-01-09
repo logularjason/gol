11da6d3ab7e8f54b9fd097fa2bc98bf9774dc671
Laura Grant <laura@logular.com>
2023-01-09 13:48:07 +1100

    smell

--- food.py
+++ food.py
@@ -19,19 +19,27 @@ class Food:
     def draw(self):
         pg.draw.rect(self.screen, self.colour, [self.x-4, self.y-4, 8, 8], 2) # left, top, width, height
 
+
+
     # return distance to creature
     def distance(self, creature):
         smell = creature.dna.smell
         dx = self.x - creature.x
         dy = self.y - creature.y
-        return math.sqrt(dx * dx + dy * dy)
+        normalisedSmell = ((smell - NORMALISED_GENE_MIN) / NORMALISED_GENE_RANGE) 
+        smellCostFactor = (1 - normalisedSmell) * SMELL_FACTOR + (1 - SMELL_FACTOR)
+        distance = math.sqrt(dx * dx + dy * dy)
+        #print ("smell =", smell, "normalisedSmell = ", normalisedSmell, " smellCostFactor =", smellCostFactor)
+        return distance * smellCostFactor 
 
     # return the direction of the food
     def direction(self, creature):
         smell = creature.dna.smell
         dx = self.x - creature.x
         dy = self.y - creature.y
-        return math.atan2(dy, dx)
+        normalisedSmell = ((smell - NORMALISED_GENE_MIN) / NORMALISED_GENE_RANGE) 
+        smellCostFactor = (1 - normalisedSmell) * SMELL_FACTOR + (1 - SMELL_FACTOR)
+        return math.atan2(dy, dx)+ (smellCostFactor * random.randint(-1, 1))
 
 class Foodlist:
     def __init__(self, screen, creatureList):
